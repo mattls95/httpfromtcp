9a4ab73365a09d7d9522630a33e5dce6e2142f8e
mattls <mattias.swenne@gmail.com>
2025-10-07 19:04:37 +0200

    Finished CH1 L5

--- main.go
+++ main.go
@@ -5,7 +5,36 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go receiveLine(ch, f)
+	return ch
+}
+
+func receiveLine(ch chan string, f io.ReadCloser) {
+	b := make([]byte, 8)
+	currentLine := ""
+	for {
+		n, err := f.Read(b)
+		parts := strings.Split(string(b[:n]), "\n")
+		for i := 0; i < len(parts)-1; i++ {
+			currentLine += parts[i]
+			ch <- currentLine
+			currentLine = ""
+		}
+		currentLine += parts[len(parts)-1]
+		if err == io.EOF {
+			if currentLine != "" {
+				ch <- currentLine
+			}
+			close(ch)
+			break
+		}
+	}
+}
 
 func main() {
 	fileName := "messages.txt"
@@ -13,18 +42,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to open file, %s", fileName)
 	}
-
-	b := make([]byte, 8)
-	for {
-		n, err := file.Read(b)
-		if n > 0 {
-			fmt.Printf("read: %s\n", b[:n])
-		}
-		if err == io.EOF {
-			os.Exit(0)
-		}
-		if err != io.EOF && err != nil {
-			log.Fatalf("Failed to read file, %s", fileName)
-		}
+	ch := getLinesChannel(file)
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
 	}
 }
